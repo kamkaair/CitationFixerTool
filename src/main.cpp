a5396550ddbd9dfea7e0ff85f53dc3612e7abcea
AlttiAir <alttiairaksinen@kamk.fi>
2026-03-09 13:54:08 +0200

    More logic for the main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <duckx.hpp>
 #include <windows.h>
+#include <string>
 //#include <bits/stdc++.h>
 
 int get_letters(std::string par, std::string &container, int index) {
@@ -11,18 +12,21 @@ int get_letters(std::string par, std::string &container, int index) {
     return get_letters(par, container, index-1);
 }
 
-void read_paragraph(std::string par) {
+void read_paragraph(std::string par, int indices[]) {
     std::string container;
     bool whileLoop = true;
-    int indices[2] = {0,0};
     
     if (par.find(']') == std::string::npos)
         return;
 
     for (int i = par.size(); whileLoop; i--) {
         if (par[i] == ']') {
-            for (auto& index : indices) {
+            /*for (auto& index : indices) {
                 index = i;
+            }*/
+
+            for (int j = 0; j < 2; j++) {
+                indices[j] = i;
             }
 
             indices[1] = get_letters(par, container, indices[1]-1);     // Go through all the letters from ']' onwards
@@ -35,6 +39,15 @@ void read_paragraph(std::string par) {
     }
 }
 
+void insert_char(std::string &inText, int indices[2]) {
+    int endPoint = indices[1]-1;
+    inText.insert(endPoint, ".");
+}
+
+void edit_document() {
+
+}
+
 int main() {
     // UTF8 text format
     SetConsoleOutputCP(CP_UTF8);
@@ -43,17 +56,25 @@ int main() {
     duckx::Document doc(ASSET_DIR + std::string("/my_test.docx"));
     doc.open();
     std::string textCache;
+    int indices[2] = { 0,0 };   // Begin, end
    
     for (auto p = doc.paragraphs(); p.has_next(); p.next()) {
         std::string paragraph;
         for (auto r = p.runs(); r.has_next(); r.next()) {
             paragraph += r.getAll_text();
+            std::cout << r.getAll_text() << std::endl;
+            //r.set_citation("aaa");
         }   
-        //textCache += '\n';
-        read_paragraph(paragraph);
+        
+        read_paragraph(paragraph, indices);
+        
+        
+
         paragraph += "\n\n";
-        std::cout << paragraph << std::endl;
+        //std::cout << paragraph << std::endl;
     }
+
+    doc.save();
 
     //cout << textCache << endl;
     return 0;
